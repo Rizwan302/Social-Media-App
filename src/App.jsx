a79e5cad4175f877c1408f3d6bc59086cbe992f8
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-03-25 16:23:23 +0500

    Add tobar file set

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Home from './page/home/Home'
 // import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Home />
     </div>
   )
 }
---
+++ src/page/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import {LockOpenIcon} from '@mui/icons-material/LockOpen';
+import { FaBeer } from 'react-icons/fa';
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* <LockOpenIcon /> */}
+      <FaBeer />
+    </div>
+  )
+}
+
+export default Home
