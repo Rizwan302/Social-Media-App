fc44a1738c3f13320362f433573c927e7798a17a
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-04-01 14:45:56 +0500

    Add css style topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Topbar.css";
+import { BiSearch } from "react-icons/bi";
+import { BsFillPersonFill, BsFillChatLeftTextFill } from "react-icons/bs";
+import { IoIosNotifications } from "react-icons/io";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbatLeft">
+        <span className="logo">My App</span>
+      </div>
+      <div className="topbatCenter">
+        <div className="searchbar">
+          <BiSearch />
+          <input type="search" placeholder="Search" className="searchInput" />
+        </div>
+      </div>
+      <div className="topbatRight">
+        <div className="topbarLinke">
+          <span className="">HomePage</span>
+          <span className="">TimeLine</span>
+        </div>
+        <div className="topbarIcone">
+          <div className="topbarIconeItem">
+            <BsFillPersonFill />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconeItem">
+            <BsFillChatLeftTextFill />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconeItem">
+            <IoIosNotifications />
+            <span className="topbarIconBadge">10</span>
+          </div>
+        </div>
+        <img src="/assets/parson/Zaira.jpeg" alt="" className="topbarImage" />
+      </div>
+    </div>
+  );
+}
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 // import {LockOpenIcon} from '@mui/icons-material/LockOpen';
 import { FaBeer } from 'react-icons/fa';
+import Topbar from '../../components/topbar/Topbar';
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      {/* <LockOpenIcon /> */}
-      <FaBeer />
+
+      <Topbar />
     </div>
   )
 }
