9f9014f274ed9be28d262872d235fd098cf13d18
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-04-03 02:31:35 +0500

    Add css style

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './Post.css'
+import { GrMoreVertical } from "react-icons/gr";
+import { AiFillLike } from "react-icons/ai";
+import { FcLike } from "react-icons/fc";
+
+
+
 function Post() {
   return (
     <div className='post'>
@@ -11,17 +17,17 @@ function Post() {
                     <span className="postDate">5 min</span>
                 </div>
                 <div className="postTopRight">
-                    icon
+                    <GrMoreVertical/>
                 </div>
             </div>
             <div className="postCenter">
                 <span className="PostText">My Post</span>
-                <img src="/assets/parson/Zaira.jpeg" alt="" />
+                <img src="/assets/parson/Zaira.jpeg" alt="" className='postImg' />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img src="" alt="" className="likeIcon" />
-                    <img src="" alt="" className="likeIcon" />
+                    <AiFillLike className="likeIcon" />
+                    <FcLike className="likeIcon" />
                     <span className="postLikeCounter">123 like</span>
                 </div>
                 <div className="postBottomRight">
