2844bfbce244702f181309be75922f5962091258
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-04-04 00:27:05 +0500

    Set right bar

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import './Rightbar.css'
-
+// import { BiSearch } from "react-icons/bi";
 
 function Rightbar() {
   return (
     <div className='rightbar'>
       <div className="rightbarWrapper">
         <div className="brithdayContainer">
-          <img src="" alt="" className="brithdayImg" />
-          <span className="brithdaytext"><b>Hello Friend</b> and <b> 3 other friend </b> have a bright today</span>
+          <img src="/assets/parson/Zaira.jpeg" alt="" className="brithdayImg" />
+          <span className="brithdaytext"><b>Hello Friend</b> and <b> 3 other friend </b> have a bright today
+          </span>
         </div>
+
+
+        <img src="/assets/parson/Zaira.jpeg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friend</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightBarFriend">
+            <div className="rightbarprofileContainer">
+              <img src="/assets/parson/Zaira.jpeg" alt="" className="rightbarprofileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUseName">My Friend</span>
+
+          </li>
+        </ul>
+
       </div>
     </div>
   )
