2e4fa6c4b8748f3ada399eb5f3f9e78c1f60b994
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-04-06 01:21:56 +0500

    Add Like Statte

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Home from './page/home/Home'
 import Profile from './components/profile/Profile'
+import Login from './page/login/Login'
 // import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <Profile />
+      {/* <Profile /> */}
+      <Login />
     </div>
   )
 }
---
+++ src/page/login/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='login'>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+            <h3 className="loginLogo">My App</h3>
+            <span className="loginDecs">
+                Connect with friend and the world around you on My App 
+            </span>
+        </div>
+        <div className="loginRight">
+            <div className="loginBox">
+                
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
